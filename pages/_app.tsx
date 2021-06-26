567b99aae00c7cf4d5f10bfd0accd184c58118ae
mohammad aquib <yatooaquib@gmail.com>
2021-06-26 22:38:00 +0530

    feat: add chakra and error boundry

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import { withErrorBoundary } from 'react-error-boundary'
+import { FullPageErrorFallback } from 'src/components/lib'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
-export default MyApp
+export default withErrorBoundary(MyApp, {
+  FallbackComponent: FullPageErrorFallback
+})
