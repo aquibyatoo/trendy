7343d580e13b6a9a8e4ddbacb03f1e31dacbafe6
mohammad aquib <yatooaquib@gmail.com>
2021-06-27 00:15:43 +0530

    refactor: remove custom css from fallback error

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,19 +1,15 @@
 import * as React from 'react'
+import { Box, Flex } from '@chakra-ui/react'
 
 function FullPageErrorFallback() {
   return (
-    <div
+    <Box
       role="alert"
-      style={{
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
     >
-      <p>Uh oh... There's a problem. Try refreshing the app.</p>
-    </div>
+      <Flex flexDirection='column' justifyContent='center' alignItems='center' h={'100vh'}>
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+      </Flex>
+    </Box>
   )
 }
 
