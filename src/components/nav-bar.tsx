2c86ea3c56ee8b0cd9f395f1ff1c02a9609900f1
mohammad aquib <yatooaquib@gmail.com>
2021-06-27 10:10:35 +0530

    feat: chnage name to Couture

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
   return (
     <Flex flexDir='row' justifyContent='space-between' alignItems='center' p='2rem' backgroundColor='#FFA8E2'>
       <Icon fill='white' as={MdMenu} cursor='pointer' w={30} h={30} />
-      <Text fontSize="4xl" as="samp">Trendy</Text>
+      <Text fontSize="4xl" as="samp">Couture</Text>
       <Cart />
     </Flex>
   )
